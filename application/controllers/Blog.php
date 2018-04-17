a5b1cf364c80f17c139a86359a445d728e0646ff
mohberliannusantara <liannusantara1158@gmail.com>
2018-04-17 21:18:36 +0000

    membuat method update_view dan delete untuk melakukan delete dan update data serta membuat validasi pada blog_add_view.php dan blog_update_view.php

--- application/controllers/Blog.php
+++ application/controllers/Blog.php
@@ -11,15 +11,50 @@ class Blog extends CI_Controller {
 
   public function add_view() {
     $data['error'] = "";
-    $this->load->view('blog_add_view', $data);
+    $this->load->helper(array('form', 'url'));
+    $this->load->library('form_validation');
+    $this->form_validation->set_rules('id', 'Id', 'required');
+    $this->form_validation->set_rules('author', 'Author', 'required');
+    $this->form_validation->set_rules('title', 'Title', 'required');
+
+    if ($this->form_validation->run() == FALSE){
+      $this->load->view('blog_add_view',$data);
+    }
+    else {
+      $config['upload_path'] = './uploads/';
+      $config['allowed_types'] = 'gif|jpg|png';
+      $config['max_size'] = 80000;
+      $config['max_width'] = 1024;
+      $config['max_height'] = 768;
+      $this->load->library('upload', $config);
+      $this->upload->initialize($config);
+
+      if ( ! $this->upload->do_upload('image_file')) {
+        $error = array('error' => $this->upload->display_errors());
+        $this->load->view('blog_add_view', $error);
+      }
+      else {
+        $dataUpload = $this->upload->data();
+        $data = array(
+        'id' => $this->input->post('id'),
+        'author' => $this->input->post('author'),
+        'date' => $this->input->post('date'),
+        'title' => $this->input->post('title'),
+        'content' => $this->input->post('content'),
+        'image_file' => $dataUpload['file_name']
+        );
+        $this->Blog_model->insert($data);
+        redirect('index.php/Blog');
+      }
+    }
   }
 
   public function add_action() {
     $config['upload_path']   = './uploads/';
     $config['allowed_types'] = 'gif|jpg|png';
     $config['max_size']      = 80000;
-    $config['max_width']     = 1024;
-    $config['max_height']    = 768;
+    $config['max_width']     = 4000;
+    $config['max_height']    = 4000;
     $this->load->library('upload', $config);
     $this->upload->initialize($config);
 
@@ -43,9 +78,55 @@ class Blog extends CI_Controller {
     }
   }
 
-  public function byId($id){
+  public function byId($id) { //method untuk menampilkan blog_view
   	 $data['records'] = $this->Blog_model->getOne($id);
      $this->load->view('blog_view',$data);
   }
 
+  public function update_view($id) { //method update
+    $data['error'] = "";
+    $this->load->helper(array('form', 'url'));
+    $this->load->library('form_validation');
+    $this->form_validation->set_rules('id', 'Id', 'required');
+    $this->form_validation->set_rules('author', 'Author', 'required');
+    $this->form_validation->set_rules('title', 'Title', 'required');
+    $data['records'] = $this->Blog_model->getOne($id);
+
+    if ($this->form_validation->run() == FALSE) {
+      $this->load->view('blog_update_view',$data);
+    }
+    else {
+      $config['upload_path'] = './uploads/';
+      $config['allowed_types'] = 'gif|jpg|png';
+      $config['max_size']      = 80000;
+      $config['max_width']     = 4000;
+      $config['max_height']    = 4000;
+      $this->load->library('upload', $config);
+      $this->upload->initialize($config);
+
+      if ( ! $this->upload->do_upload('image_file')) {
+        $data['error'] = $this->upload->display_errors();
+        $this->load->view('blog_update_view', $data);
+      }
+      else {
+        $dataUpload = $this->upload->data();
+        $data = array(
+        'id' => $this->input->post('id'),
+        'author' => $this->input->post('author'),
+        'date' => $this->input->post('date'),
+        'title' => $this->input->post('title'),
+        'content' => $this->input->post('content'),
+        'image_file' => $dataUpload['file_name']
+        );
+        $old_id = $this->input->post('old_id');
+        $this->Blog_model->update($data,$old_id);
+        redirect('index.php/Blog');
+      }
+    }
+  }
+
+  public function delete_action($id) { //method delete
+    $this->Blog_model->delete($id);
+    redirect('index.php/Blog');
+  }
 }
--- application/views/blog_update_view.php
+++ application/views/blog_update_view.php
@@ -7,7 +7,7 @@
   </head>
   <body>
     <nav class="navbar navbar-expand-md navbar-dark bg-dark mb-4">
-      <a class="navbar-brand" href="#">Top navbar</a>
+      <a class="navbar-brand" href="#">Lian_N</a>
       <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarCollapse" aria-controls="navbarCollapse" aria-expanded="false" aria-label="Toggle navigation">
         <span class="navbar-toggler-icon"></span>
       </button>
@@ -32,43 +32,44 @@
       </div>
     </nav>
     <main role="main" class="container">
-      <a href="<?php echo base_url("index.php/Blog/add_view") ?>" class="btn btn-primary">Tambah Blog</a>
-      <br>
-      <br>
-      <ul class="list-unstyled">
-      <?php foreach ($records as $key => $value): ?>
-        <li class="media">
-          <a href="<?php echo base_url('index.php/Blog/byId/'.$value['id']) ?>">
-            <img src="<?php echo base_url() ?>uploads/<?php echo $records[0]['image_file']  ?>" alt="Generic placeholder image" width="200px" height="200px" class="">
-          </a>
-          <div class="media-body">
-            <table class="table">
-              <tr>
-                <td><h6 class="text-muted"><?php echo $value['date'] ?></h6></td>
-              </tr>
-              <tr>
-                <td><h5 class="mt-0 mb-1"><?php echo $value['title'] ?></h5></td>
-              </tr>
-              <tr>
-                <td><?php echo $value['content'] ?></td>
-              </tr>
-              <tr>
-                <td><a href="<?php echo base_url('index.php/Blog/byId/'.$value['id']) ?>">View Details</a></td>
-              </tr>
-              <tr>
-                <td>
-                  <a class="btn btn-sm btn-success" href="<?php echo base_url('index.php/Blog/update_view/'.$value['id']) ?>">Update</a>
-                  <a class="btn btn-sm btn-danger" href="<?php echo base_url('index.php/Blog/delete_action/'.$value['id']) ?>">Delete	</a>
-              </td>
-              </tr>
-            </table>
+      <div class="row">
+        <div class="col">
+        <?php echo $error ?>
+        <?php echo validation_errors() ?>
+        <?php echo form_open_multipart('index.php/Blog/update_view/'.$records[0]['id']);?>
+          <div class="form-group">
+            <label for="id">Id</label>
+            <input type="text" class="form-control" id="id" name="id" value="<?php echo $records[0]['id'] ?>">
+            <input type="hidden" name="old_id" value="<?php echo $records[0]['id']?>">
           </div>
-      </li>
-      <?php endforeach ?>
-      </ul>
+          <div class="form-group">
+            <label for="author">Author</label>
+            <input type="text" class="form-control" id="author" name="author" value="<?php echo $records[0]['author'] ?>">
+          </div>
+          <div class="form-group">
+            <label for="date">Date</label>
+            <input type="date" class="form-control" id="date" name="date" value="<?php echo $records[0]['date'] ?>">
+          </div>
+          <div class="form-group">
+            <label for="title">Title</label>
+            <input type="text" class="form-control" id="title" name="title" value="<?php echo $records[0]['title'] ?>">
+          </div>
+          <div class="form-group">
+            <label for="content">Content</label>
+            <input type="text" class="form-control" id="content" name="content" value="<?php echo $records[0]['content'] ?>">
+          </div>
+            <img src="<?php echo base_url(); ?>/uploads/<?php echo $records[0]['image_file'] ?>" alt="File Tidak Ada" height="100px" width="100px">
+            <div class="form-group">
+            <label for="image_file">Image</label>
+            <input type = "file" name = "image_file" size = "20" />
+          </div>
+          <input type="submit" name="update" value="Update" class="btn btn-success">
+          </form>
+        </div>
+      </div>
     </main>
     <script src="https://code.jquery.com/jquery-3.2.1.slim.min.js" integrity="sha384-KJ3o2DKtIkvYIK3UENzmM7KCkRr/rE9/Qpg6aAZGJwFDMVNA/GpGFF93hXpG5KkN" crossorigin="anonymous"></script>
     <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.12.9/umd/popper.min.js" integrity="sha384-ApNbgh9B+Y1QKtv3Rn7W3mgPxhU9K/ScQsAP7hUibX39j7fakFPskvXusvfa0b4Q" crossorigin="anonymous"></script>
     <script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/js/bootstrap.min.js" integrity="sha384-JZR6Spejh4U02d8jOt6vLEHfe/JQGiRRSQQxSfFWpi1MquVdAyjUar5+76PVCmYl" crossorigin="anonymous"></script>
-  </body>
+  </body>s
 </html>
